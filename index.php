b367c9949aeafb03c701a6187a65ac5d8d05e6ae
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-04-10 20:58:23 -0400

    Display the view of home page

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sjawa
- * Date: 4/8/2019
- * Time: 5:24 PM
+ * Name: Sukhveer S Jawandha
+ * 4/8/2019
+ * 328/howdy/index.php
  */
+
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once ('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+// Define a default route
+$f3->route('GET /' , function (){
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+//Run Fat-Free
+$f3->run();
