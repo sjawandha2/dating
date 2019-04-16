20e9b54487235b9f01074aacded1b9c99d847867
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-04-16 14:49:46 -0400

    Added the personal info form and route of page

--- index.php
+++ index.php
@@ -14,17 +14,23 @@ require_once ('vendor/autoload.php');
 
 //create an instance of the Base class
 $f3 = Base::instance();
-//Turn on Fat-Free error reporting
-$f3->set('DEBUG', 3);
+////Turn on Fat-Free error reporting
+//$f3->set('DEBUG', 3);
 
 // Define a default route
 $f3->route('GET /' , function (){
 
-    //Display a view
+    //Display a views
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
+// Define a route with a parameter
+$f3 ->route('GET /signup' ,function(){
 
+    //Display a views
+    $view = new Template();
+    echo $view->render('views/info.html');
+});
 //Run Fat-Free
 $f3->run();
