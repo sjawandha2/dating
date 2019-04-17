813eed97ef12f54fa25d65688b0cc2deed2e03c1
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-04-17 14:11:20 -0400

    Added profile page and added the route for pages when click next

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Name: Sukhveer S Jawandha
  * 4/8/2019
- * 328/howdy/index.php
+ * 328/dating/index.php
  */
 
 //Turn on error reporting
@@ -17,20 +17,28 @@ $f3 = Base::instance();
 ////Turn on Fat-Free error reporting
 //$f3->set('DEBUG', 3);
 
-// Define a default route
+// Define a default route for home page
 $f3->route('GET /' , function (){
 
-    //Display a views
+    //Display a view of home page
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-// Define a route with a parameter
-$f3 ->route('GET /signup' ,function(){
+// Define a route sign up information page
+$f3 ->route('GET /signup/info' ,function(){
 
     //Display a views
     $view = new Template();
     echo $view->render('views/info.html');
 });
+// when click next it goes to profile info page
+$f3 ->route('GET /signup/profile' ,function(){
+
+    //Display a views
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+
 //Run Fat-Free
 $f3->run();
