daeec90a503bfb3066806338e7e5cbcf2b494fc8
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-05-07 23:32:27 -0400

    Added the premium member class

--- classes/member.php
+++ classes/member.php
@@ -13,16 +13,12 @@ class Member
     private $_bio;
 
     // Define parameterized constructor
-    function __construct($fname, $lname, $age, $gender, $phone,$email, $state, $seeking,$bio )
+    function __construct($fname, $lname, $age, $gender, $phone)
     {
         $this->_fname = $fname;
         $this->_lname = $lname;
         $this->_age = $age;
         $this->_gender = $gender;
         $this->_phone = $phone;
-        $this->_email = $email;
-        $this->_state = $state;
-        $this->_seeking= $seeking;
-        $this->_bio = $bio;
     }
 }
---
+++ classes/premiummember.php
@@ -0,0 +1,12 @@
+<?php
+
+class PremiumMember extends Member
+{
+    private $_inDoorInterests;
+    private $_outDoorInterests;
+
+    function __construct()
+    {
+    }
+
+}
